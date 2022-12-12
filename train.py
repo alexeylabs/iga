e8b015bef829b4176cfa0ac23267583f6ad89a30
alexey <alexeylabs@gmail.com>
2022-12-12 21:37:57 +0300

    added best model parameter

--- train.py
+++ train.py
@@ -40,6 +40,7 @@ def train(model: IGA,
     print_each = 500
     images_to_save = 8
     saved_images_size = (512, 512)
+    best_ber = 0.5
 
     for epoch in tqdm(range(train_options.start_epoch, train_options.number_of_epochs + 1)):
         logging.info('\nStarting epoch {}/{}'.format(epoch, train_options.number_of_epochs))
@@ -91,6 +92,11 @@ def train(model: IGA,
 
         utils.log_progress(validation_losses)
         logging.info('-' * 40)
-        utils.save_checkpoint(model, train_options.experiment_name, epoch, os.path.join(this_run_folder, 'checkpoints'))
+
+        new_ber = validation_losses['bitwise-error  ']
+        if new_ber < best_ber:
+            best_ber = new_ber
+            utils.save_checkpoint(model, train_options.experiment_name, epoch, os.path.join(this_run_folder, 'checkpoints'))
+
         utils.write_losses(os.path.join(this_run_folder, 'validation.csv'), validation_losses, epoch,
                            time.time() - epoch_start)
